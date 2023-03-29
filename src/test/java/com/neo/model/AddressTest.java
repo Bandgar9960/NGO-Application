7233e1a6fbcf12224e71a11c3ded85067e041a8a
Dnyaneshwar Bandgar <Bandgar9960@gmail.com>
2023-03-29 20:55:33 +0530

    Address and Admin Test Done

---
+++ src/test/java/com/neo/model/AddressTest.java
@@ -0,0 +1,48 @@
+package com.neo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void getCity() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void getCountry() {
+    }
+
+    @Test
+    void getPincode() {
+    }
+
+    @Test
+    void setArea() {
+    }
+
+    @Test
+    void setCity() {
+    }
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void setCountry() {
+    }
+
+    @Test
+    void setPincode() {
+    }
+}
---
+++ src/test/java/com/neo/model/AdminTest.java
@@ -0,0 +1,64 @@
+package com.neo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+
+    @Test
+    void getAdminId() {
+    }
+
+    @Test
+    void getAdminName() {
+    }
+
+    @Test
+    void getAdminContact() {
+    }
+
+    @Test
+    void getAdminAadhar() {
+    }
+
+    @Test
+    void getAdminGender() {
+    }
+
+    @Test
+    void getLogin() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setAdminId() {
+    }
+
+    @Test
+    void setAdminName() {
+    }
+
+    @Test
+    void setAdminContact() {
+    }
+
+    @Test
+    void setAdminAadhar() {
+    }
+
+    @Test
+    void setAdminGender() {
+    }
+
+    @Test
+    void setLogin() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+}
